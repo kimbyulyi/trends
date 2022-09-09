11616dd4c56adc4a0d0cb3a2be84fd13d1e7d6eb
kimbyulyi <byulyikeem@gmail.com>
2022-09-09 22:38:44 +0900

    feat: integrate prisma and partially implements registration

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,3 +1,13 @@
+import db from "../lib/db.js";
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
+
+interface AUthParams {
+  username: string;
+  password: string;
+}
+
 class UserService {
   private static instance: UserService;
   public static getInstance() {
@@ -7,8 +17,25 @@ class UserService {
     return UserService.instance;
   }
 
-  register() {
-    return "registered!";
+  async register({ username, password }: AUthParams) {
+    const exists = await db.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (exists) {
+      throw new Error("User Already exists");
+    }
+
+    const hash = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = db.user.create({
+      data: {
+        username,
+        passwordHash: hash,
+      },
+    });
+    return user;
   }
 
   login() {
